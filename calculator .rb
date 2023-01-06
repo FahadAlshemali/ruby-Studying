33f295a55a6ba66f3d9a7b4fb9357647a704ee09
FahadAlshemali <fhadfhad222@gmail.com>
2023-01-06 13:51:25 +0200

    first commit

--- calculator .rb
+++ calculator .rb
@@ -1,21 +1,36 @@
+puts 'Enter first number: '
+
+num1 = gets.chomp.to_f
+
+puts 'Enter operator'
+
+op = gets.chomp
+
+puts 'Enter second number: '
+
+num2 = gets.chomp.to_f
 
 
-puts "Enter first number: "
-num1 = gets.chomp().to_f
-puts "Enter operator"
-op = gets.chomp()
-puts "Enter second number: "
-num2 = gets.chomp().to_f
 
+if op == '+'
 
-if op == "+" 
-   puts (num1 + num2)
-    elsif op == "-"
-    puts (num1 - num2)
-    elsif op == "*"
-    puts (num1 * num2)
-    elsif op == "/"
-        puts (num1 / num2)
-    else 
-    puts "Invalid operetor "
+  puts(num1 + num2)
+
+elsif op == '-'
+
+  puts(num1 - num2)
+
+elsif op == '*'
+
+  puts(num1 * num2)
+
+elsif op == '/'
+
+  puts(num1 / num2)
+
+else
+
+  puts 'Invalid operetor '
+
 end
+
